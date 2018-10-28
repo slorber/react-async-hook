e7604cf3881d037697790433cfbb6ed81db1866c
slorber <lorber.sebastien@gmail.com>
2018-10-28 12:05:22 +0100

    better example

--- example/src/App.js
+++ example/src/App.js
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
 
 import {
   useAsync,
 } from "react-async-hook";
 
 const fetchStarwarsHero = id => fetch(`https://swapi.co/api/people/${id}/`).then(result => {
-  if ( result.status !== 200 ) {
+  if (result.status !== 200) {
     throw new Error("bad status = " + result.status);
   }
   return result.json();
@@ -20,28 +20,61 @@ const buttonStyle = {
 };
 
 const App = () => {
-  const [heroId,setHeroId] = useState(1);
+  const [heroId, setHeroId] = useState(1);
   const next = () => setHeroId(heroId + 1);
   const previous = () => setHeroId(heroId - 1);
   return (
-    <div style={{ padding: 20 }}>
-      <div style={{margin: 20, display: "flex"}}>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+    >
+
+      <div style={{display: "flex"}}>
         <div style={buttonStyle} onClick={previous}>Previous</div>
         <div style={buttonStyle} onClick={next}>Next</div>
       </div>
-      <div style={{margin: 20}}>Current hero id={heroId}</div>
-      <div style={{margin: 20}}>
-        <StarwarsHero id={heroId}/>
+
+      <div style={{display: "flex", marginTop: 50}}>
+        <HeroContainer>
+          <StarwarsHero id={heroId}/>
+        </HeroContainer>
+        <HeroContainer>
+          <StarwarsHero id={heroId + 1}/>
+        </HeroContainer>
+        <HeroContainer>
+          <StarwarsHero id={heroId + 2}/>
+        </HeroContainer>
       </div>
+
     </div>
   );
 };
 
 
+const HeroContainer = ({children}) => (
+  <div style={{
+    margin: 10,
+    padding: 10,
+    border: "solid",
+    borderRadius: 10,
+    width: 200,
+    height: 80,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center"
+  }}>
+    {children}
+  </div>
+);
+
+
 const StarwarsHero = ({id}) => {
-
   const asyncHero = useAsync(fetchStarwarsHero, [id]);
-
   return (
     <div>
       {asyncHero.loading && (
@@ -53,6 +86,7 @@ const StarwarsHero = ({id}) => {
       {asyncHero.result && (
         <div>
           <div>Success!</div>
+          <div>Id: {id}</div>
           <div>Name: {asyncHero.result.name}</div>
         </div>
       )}
