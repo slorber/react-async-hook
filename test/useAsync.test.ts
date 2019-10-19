c5b52636ccdc91a8c95f0abcd2bc315b1b1e5148
Paul Cowan <paul.cowan@cutting.scot>
2019-10-19 16:26:24 +0100

    add error tests

--- test/useAsync.test.ts
+++ test/useAsync.test.ts
@@ -33,8 +33,6 @@ describe('useAync', () => {
 
   beforeEach(() => {
     fetch.resetMocks();
-
-    fetch.mockResponseOnce(JSON.stringify(fakeResults));
   });
 
   it('should have a useAsync hook', () => {
@@ -42,6 +40,8 @@ describe('useAync', () => {
   });
 
   it('should set loading flag when request is initially made', () => {
+    fetch.mockResponseOnce(JSON.stringify(fakeResults));
+
     const { result } = renderHook<
       StarwarsHeroArgs,
       UseAsyncReturn<StarwarsHero[]>
@@ -55,6 +55,8 @@ describe('useAync', () => {
   });
 
   it('should resolve a successful request', async () => {
+    fetch.mockResponseOnce(JSON.stringify(fakeResults));
+
     const { result, waitForNextUpdate } = renderHook<
       StarwarsHeroArgs,
       UseAsyncReturn<StarwarsHero[]>
@@ -67,5 +69,23 @@ describe('useAync', () => {
     expect(result.current.result).toEqual(fakeResults);
     expect(result.current.loading).toBe(false);
     expect(result.current.error).toBeUndefined();
+  });
+
+  it('should set error detail for unsuccessful request', async () => {
+    fetch.mockReject(new Error('something went wrong'));
+
+    const { result, waitForNextUpdate } = renderHook<
+      StarwarsHeroArgs,
+      UseAsyncReturn<StarwarsHero[]>
+    >(p => useAsync<StarwarsHero[], any>(p.asyncFunction, []), {
+      initialProps: { ...props },
+    });
+
+    await waitForNextUpdate();
+
+    expect(result.current.error).toBeDefined();
+    expect(result.current.error!.message).toBe('something went wrong');
+    expect(result.current.loading).toBe(false);
+    expect(result.current.result).toBeUndefined();
   });
 });
